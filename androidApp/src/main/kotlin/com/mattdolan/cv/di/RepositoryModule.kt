de6adccae59c48030f15b1b84dd3bec8f61e830e
Matthew Dolan <info@mattdolan.com>
2021-01-26 09:39:40 +0000

    Update ViewModels to latest @HiltViewModel annotation (#1)

--- androidApp/src/main/kotlin/com/mattdolan/cv/di/RepositoryModule.kt
+++ androidApp/src/main/kotlin/com/mattdolan/cv/di/RepositoryModule.kt
@@ -21,14 +21,17 @@ import com.mattdolan.cv.Sdk
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 object RepositoryModule {
 
     @Provides
+    @ViewModelScoped
     fun provideProfileRepository(
         @ApplicationContext context: Context
     ) = Sdk(context).profileRepository()
